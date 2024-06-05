2b118f048b4edf28974429271a3b50771958cd18
Jorgen Eilertsen <jorgen.th.eilertsen@gmail.com>
2024-06-05 08:44:15 +0200

    Fixed errors, added documentation, demo gif and executable

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# autoclicker
-Mouse auto clicker in Python
+# Autoclicker
+
+Simple autoclicker written in Python
+
+## In-Depth Function
+
+This as-simple-as-possible python script simply makes the mouse click one time every minute. This can run for any given stretch of time, provivided by the user. The program runs in terminal.
+
+## Getting Started
+
+### Dependencies
+
+Just windows! You can run the executable on any windows computer.
+
+If you want to run in Python:
+* Python 
+* Python Libraries: pyautogui, time, datetime
+
+### Installing
+
+* Download from browser or git clone
+
+### Executing program
+
+On windows you can run the provided executable
+
+To run the python project you can double press the autoclicker.py-script. 
+
+![program demo gif](https://github.com/Akonisunderrated/autoclicker/blob/main/README.mddemo/demo.gif)
+
+You can also use the *clicker()* function independently. This function accepts any number of hours as input and runs for that amount of time.
+
+## Help
+
+**To terminate the program before the given time:**
+```
+Ctrl+C
+```
+
+## Authors
+
+Jørgen Eilertsen 
+[@gmail](mailto:jorgen.th.eilertsen@gmail.com)
+
+## Version History
+* 1.1
+    * Added project documentation, demo gif and windows executeble. Repaired encoding errors in inital release
+* 1.0
+    * Initial Release
+
+## License
+
+Use this in any way you want
+
+## Acknowledgments
+
+I am sure this has been done a million times, and that I may have seen inspiration somwhere. Please use this project as you wish
--- autoclicker.py
+++ autoclicker.py
@@ -1,16 +1,17 @@
-# AutoClicker V1.0 
-# Updated 05June2024
-# Jørgen Eilertsen
-# httpsgithub.comAkonisunderrated
+# -*- coding: utf-8 -*-
+# AutoClicker V1.1 
+# Updated: 05/June/2024
+# Jorgen Eilertsen
+# https://github.com/Akonisunderrated
 
 import pyautogui
 import time
 from datetime import datetime
 
 # Main clicker function. 
-#Will click where ever the mouse pointer is. 
-#Once every 1 minute for the amount of hours given.
-def clicker(x)
+# Will click where ever the mouse pointer is. 
+# Once every 1 minute for the amount of hours given.
+def clicker(x):
     
     print('Countdown')
     print('5')
@@ -26,32 +27,32 @@ def clicker(x)
     print('Clicking')
 
     c = 0
-    while c  x60
+    while c < x*60:
         pyautogui.click()
         c += 1
         
         now = datetime.now()
 
-        current_time = now.strftime(%H%M%S)
+        current_time = now.strftime("%H:%M:%S")
         print(c, ' at ', current_time)
         time.sleep(60)
     print('done at ', current_time)
 
-
-def run_routine()
-    click_run_length_hours = int(input('Please input amount of hours to run  '))
+# simple run routine to allow for input to control hours runtime.
+def run_routine():
+    click_run_length_hours = int(input('Please input amount of hours to run : '))
     clicker(click_run_length_hours)
-    rerun = input('run again (yn)')
-    if rerun.lower() == 'n'
+    rerun = input('run again? (y/n)')
+    if rerun.lower() == 'n':
         return False
-    elif rerun.lower() == 'y'
+    elif rerun.lower() == 'y':
         return True
-    else
+    else:
         raise ValueError('Unexpected answer. Please answer y or n')    
 
 
-
-if __name__ == '__main__'
+# Will only run if run as main script
+if __name__ == '__main__':
     run=True
-    while run
+    while run:
         run = run_routine()
